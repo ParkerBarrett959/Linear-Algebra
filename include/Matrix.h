55199b0bfb2755b9605264b1dc1c6bf76b240cc7
Parker Barrett <parkerb7@vt.edu>
2024-05-27 11:35:25 -0400

    Add constructords and destructors

--- include/Matrix.h
+++ include/Matrix.h
@@ -13,6 +13,20 @@ template <typename T>
 class Matrix {
  public:
     
+    // Default Constructor
+    Matrix() : nRows_(0), nCols_(0) {}
+
+    // Constuct Matrix of Size with Specific Value
+    Matrix(unsigned rows, unsigned cols, T val = 0) :
+        nRows_(rows), nCols_(cols) {
+	data_resize(nRows_);
+        for (unsigned i = 0; i < data_.size(); i++) {
+	    data_[i].resize(nCols_, val);
+	}
+    }
+
+    // Destructor
+    ~Matrix() {}
 
  private:
     // Matrix Data
