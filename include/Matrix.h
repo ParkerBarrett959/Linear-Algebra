e2e02e96c611909f452d2e13b7cd3d0d69497a64
Parker Barrett <parkerb7@vt.edu>
2024-05-27 12:21:45 -0400

    Add getters for rows and columns

--- include/Matrix.h
+++ include/Matrix.h
@@ -15,7 +15,7 @@ class Matrix {
   // Default Constructor
   Matrix() : nRows_(0), nCols_(0) {}
 
-  // Constuct Matrix of Size with Specific Value
+  // Construct Matrix of Size with Specific Value
   Matrix(unsigned rows, unsigned cols, T val = 0) : nRows_(rows), nCols_(cols) {
     data_.resize(nRows_);
     for (unsigned i = 0; i < data_.size(); i++) {
@@ -25,6 +25,12 @@ class Matrix {
 
   // Destructor
   ~Matrix() {}
+
+  // Get Number of Rows
+  unsigned rows() { return nRows_; }
+
+  // Get Number of Columns
+  unsigned cols() { return nCols_; }
 
  private:
   // Matrix Data
--- test/MatrixTest.cpp
+++ test/MatrixTest.cpp
@@ -16,5 +16,11 @@ TEST(MatrixConstructor, Default) {
   Matrix<double> matrixDouble;
 
   // Check Matrix Size
-  ASSERT_EQ(0, 0);
+  ASSERT_EQ(matrixInt.rows(), 0);
+  ASSERT_EQ(matrixInt.cols(), 0);
+  ASSERT_EQ(matrixFloat.rows(), 0);
+  ASSERT_EQ(matrixFloat.cols(), 0);
+  ASSERT_EQ(matrixDouble.rows(), 0);
+  ASSERT_EQ(matrixDouble.cols(), 0);
+
 }
